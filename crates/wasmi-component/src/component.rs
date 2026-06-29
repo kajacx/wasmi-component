b71ca504927d7d80df95c36a6929619ba98faa68
kajacx <kajacx@gmail.com>
2026-06-29 22:39:20 +0200

    Add component parser

--- crates/wasmi-component/src/component.rs
+++ crates/wasmi-component/src/component.rs
@@ -1,10 +1,41 @@
+use wasmparser::{Parser, Payload};
+
 use crate::*;
 
 #[derive(Debug)]
-pub struct Component {}
+pub struct Component {
+    core_module: wasmi::Module,
+}
 
 impl Component {
-    pub fn new(_engine: &Engine, _bytes: &[u8]) -> Result<Self, wasmi::Error> {
-        Ok(Self {})
+    pub fn new(engine: &Engine, bytes: &[u8]) -> Result<Self, wasmi::Error> {
+        let parser = Parser::new(0);
+
+        let mut modules = Vec::with_capacity(4);
+
+        for payload in parser.parse_all(bytes) {
+            // println!("reading: {:?}", payload);
+            match payload.expect("TODO:") {
+                Payload::ModuleSection {
+                    unchecked_range, ..
+                } => {
+                    let module_bytes = &bytes[unchecked_range];
+
+                    let module_name = format!("../modules/module{}.core.wasm", modules.len());
+                    let mut file = std::fs::File::create(module_name).unwrap();
+                    std::io::Write::write_all(&mut file, module_bytes).unwrap();
+
+                    let module = wasmi::Module::new(engine, module_bytes)?;
+                    modules.push(module);
+                }
+                _ => {}
+            }
+        }
+
+        println!("Found modules: {:?}", modules.len());
+
+        Ok(Self {
+            core_module: modules.remove(0),
+        })
     }
 }
--- examples/component/primitives/run-example.sh
+++ examples/component/primitives/run-example.sh
@@ -8,7 +8,15 @@ cargo build --target wasm32-wasip2
 cd ..
 
 # wit-bindgen-wcl ./example.wit ./host/src/bindings.rs
+rm -rf modules
+mkdir modules
 
 cd host
-cargo run
+cargo run || true
+cd ..
+
+cd modules
+wasm-tools print module0.core.wasm > module0.core.wat
+wasm-tools print module1.core.wasm > module1.core.wat
+wasm-tools print module2.core.wasm > module2.core.wat
 cd ..
