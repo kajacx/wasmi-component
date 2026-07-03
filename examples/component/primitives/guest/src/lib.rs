872c4acde7007e4776540c184a624c2d20110091
kajacx <kajacx@gmail.com>
2026-07-03 03:08:09 +0200

    Add many simple roundtrip examples

--- examples/component/primitives/guest/src/lib.rs
+++ examples/component/primitives/guest/src/lib.rs
@@ -6,31 +6,38 @@ mod bindings {
     use super::GuestComponent;
     export!(GuestComponent);
 }
-
 struct GuestComponent;
 
-impl bindings::exports::wasmi_component::examples::funcs::Guest for GuestComponent {
-    fn add_s32(a: i32, b: i32) -> i32 {
-        a + b
+impl bindings::exports::wasmi_component::component_examples::common_funcs::Guest
+    for GuestComponent
+{
+    fn roundtrip_s32(value_a: i32) -> i32 {
+        bindings::wasmi_component::component_examples::common_funcs::roundtrip_s32(value_a)
     }
 
-    fn greet(name: String, count: i32) -> String {
-        format!("Hello {name}, I am a wasm component {count} times.")
+    fn roundtrip_string(value_a: String) -> String {
+        bindings::wasmi_component::component_examples::common_funcs::roundtrip_string(&value_a)
     }
 
-    fn no_args() -> () {
-        println!("Wasi when?");
+    fn roundtrip_multiple(value_a: String, value_b: i32) -> String {
+        bindings::wasmi_component::component_examples::common_funcs::roundtrip_multiple(
+            &value_a, value_b,
+        )
+    }
+
+    fn no_arguments() -> () {
+        bindings::wasmi_component::component_examples::common_funcs::no_arguments()
     }
 }
 
 impl bindings::Guest for GuestComponent {
-    fn add_u32(a: u32, b: u32) -> u32 {
-        bindings::sub_u32(a, 5) + b
+    fn add_export(value_a: u32, value_b: u32) -> u32 {
+        bindings::add_import(value_a, value_b)
     }
 }
 
-impl bindings::exports::additional::Guest for GuestComponent {
-    fn add_f32(a: f32, b: f32) -> f32 {
-        a + b
+impl bindings::exports::inline_exports::Guest for GuestComponent {
+    fn inline_add(value_a: u32, value_b: u32) -> u32 {
+        bindings::inline_imports::inline_add(value_a, value_b)
     }
 }
